1c1d9271c7d35f250e0e545a981ed404206aea24
Consciência Divinal <concienciadivinal8@gmail.com>
2026-02-04 01:03:51 -0300

    fix: import missing cn utility in Depoimentos page

--- src/pages/Depoimentos.tsx
+++ src/pages/Depoimentos.tsx
@@ -36,6 +36,7 @@ import { TOAST_MESSAGES } from '@/constants/messages';
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { formatDateBR } from '@/lib/date-utils';
+import { cn } from '@/lib/utils';
 import { 
     useDepoimentosInfinito, 
     useCerimoniasSelect, 
